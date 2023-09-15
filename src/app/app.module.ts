542350e8eaf77d2e0c81dcadc0b55fd3d15999e9
Aleksandr <ledich19@gmail.com>
2023-09-16 00:21:16 +0300

    fix: fix lint problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoItemComponent } from './video-item/video-item.component';
-import { SearchResultsComponent } from './search/search-results/search-results.component';
-import { SearchItemComponent } from './search/search-item/search-item.component';
+import HeaderComponent from './header/header/header.component';
+import SearchResultsComponent from './search/search-results/search-results.component';
+import SearchItemComponent from './search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SearchComponent,
-    VideoListComponent,
-    VideoItemComponent,
-    SearchResultsComponent,
-    SearchItemComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
