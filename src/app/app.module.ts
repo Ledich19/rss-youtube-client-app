47f750fbb7685c1a0da4795aa85988f65b081f21
Aleksandr <ledich19@gmail.com>
2023-09-16 00:31:03 +0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,22 @@ import AppComponent from './app.component';
 import HeaderComponent from './header/header/header.component';
 import SearchResultsComponent from './search/search-results/search-results.component';
 import SearchItemComponent from './search/search-item/search-item.component';
+import UserComponent from './header/user/user.component';
+import SettingComponent from './header/setting/setting.component';
+import SearchComponent from './header/search/search.component';
+import FilteringComponent from './filtering/filtering.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    UserComponent,
+    SettingComponent,
+    SearchComponent,
+    FilteringComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
