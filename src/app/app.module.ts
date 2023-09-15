d4313ca6da7ecbd8c60e99269e8af13476a14b49
Aleksandr <ledich19@gmail.com>
2023-09-16 00:18:19 +0300

    feat: generate elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoItemComponent } from './video-item/video-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    VideoListComponent,
+    VideoItemComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
