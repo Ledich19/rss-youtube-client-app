f0fe9441d233cc03a47c29e8d6dee3fa8eef46bc
Aleksandr <ledich19@gmail.com>
2023-10-07 21:31:57 +0300

    feat: add header search

--- src/app/search/search-item/search-item.component.ts
+++ src/app/search/search-item/search-item.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  faEye,
+  faHeart,
+  faHeartCircleMinus,
+  faWindowRestore,
+} from '@fortawesome/free-solid-svg-icons';
+import { IVideo } from '../search-item.model';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export default class SearchItemComponent {}
+export default class SearchItemComponent {
+  @Input() video!: IVideo;
+
+  details = false;
+
+  faEye = faEye;
+
+  faHeart = faHeart;
+
+  faHeartCircleMinus = faHeartCircleMinus;
+
+  faWindowRestore = faWindowRestore;
+}
--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import videosData from '../../../data';
+import { IVideoList } from '../search-response.model';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export default class SearchResultsComponent {}
+export default class SearchResultsComponent {
+  videos: IVideoList = videosData;
+}
